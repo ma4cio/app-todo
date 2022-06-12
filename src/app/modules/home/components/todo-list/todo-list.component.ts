d332267e041d2ea7f35e7e9b8cd50a38edaf821a
Marcio Lima <marcio.lima@bancodaycoval.com.br>
2022-06-11 23:25:14 -0300

    Hands-on 10: Implementacao do todo list e suas validacoes de entrada

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 //Interface
 import { TaskList } from '../../model/task-list';
 
@@ -7,13 +7,16 @@ import { TaskList } from '../../model/task-list';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements DoCheck {
 
   public taskList: Array<TaskList> = [];
   constructor() { }
 
-  ngOnInit(): void {
+  ngDoCheck(): void {
+    this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked));
   }
+
+
   public deleteItemTaskList(event: number) {
     this.taskList.slice(event,1)
   }
@@ -28,4 +31,15 @@ export class TodoListComponent implements OnInit {
        this.taskList = [];
       }
   }
+  public validationInput(event: string, index: number){
+    if(!event.length){
+      const confirm = window.confirm("Task está vazia, deseja realmente deletar?");
+
+    if(confirm){
+      this.deleteItemTaskList(index)
+    }
+    }
+  }
+
+
 }
