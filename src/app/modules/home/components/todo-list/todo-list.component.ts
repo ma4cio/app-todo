f2d835c1998b6dd64dc9b458daed1f930124eb27
Marcio Lima <marcio.lima@bancodaycoval.com.br>
2022-06-11 22:06:00 -0300

    Hands-on 7: Codificacao do todo-list

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+//Interface
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,6 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
+  public taskList: Array<TaskList> = [
+    { task:"Minha nova Task", checked: true },
+    { task:"Minha nova Task2", checked: false },
+
+  ];
   constructor() { }
 
   ngOnInit(): void {
